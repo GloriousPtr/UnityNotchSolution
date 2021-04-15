09ad25e55348fb932b72dc73d63f4be5148919fb
Mohit Sethi <mohitsethi.32@gmail.com>
2021-04-15 21:26:11 +0530

    :bug: Fixed a bug related to editor where anchor size increases drastically after recompilation + Better demo

--- Assets/UnityNotchSolution/Scripts/SafeArea.cs
+++ Assets/UnityNotchSolution/Scripts/SafeArea.cs
@@ -4,43 +4,50 @@
 [RequireComponent(typeof(RectTransform))]
 public class SafeArea : MonoBehaviour
 {
-    private RectTransform _rectTransform;
-    private Rect _lastSafeArea = new Rect(0, 0, 0, 0);
+	private RectTransform _rectTransform;
+	private Rect _lastSafeArea = new Rect(0, 0, 0, 0);
+	
+	private static readonly Vector2 UnitVector = Vector2.one;
+	
+	private void Awake()
+	{
+		_rectTransform = GetComponent<RectTransform>();
+		Refresh();
+	}
+	
+	#if UNITY_EDITOR
+	private void Update()
+	{
+		Refresh();
+	}
+	#endif
+	
+	private void Refresh()
+	{
+		Rect safeArea = Screen.safeArea;
 
-    private void Awake()
-    {
-        _rectTransform = GetComponent<RectTransform>();
-        Refresh();
-    }
-
-    private void Update()
-    {
-        Refresh();
-    }
-
-    private void Refresh()
-    {
-        Rect safeArea = Screen.safeArea;
-
-        if (safeArea != _lastSafeArea)
-            ApplySafeArea(safeArea);
-    }
-
-    private void ApplySafeArea(Rect r)
-    {
-        if (!_rectTransform)
-            _rectTransform = GetComponent<RectTransform>();
-        
-        _lastSafeArea = r;
-
-        // Convert safe area rectangle from absolute pixels to normalised anchor coordinates
-        Vector2 anchorMin = r.position;
-        Vector2 anchorMax = r.position + r.size;
-        anchorMin.x /= Screen.width;
-        anchorMin.y /= Screen.height;
-        anchorMax.x /= Screen.width;
-        anchorMax.y /= Screen.height;
-        _rectTransform.anchorMin = anchorMin;
-        _rectTransform.anchorMax = anchorMax;
-    }
+		if (safeArea != _lastSafeArea)
+			ApplySafeArea(safeArea);
+	}
+	
+	private void ApplySafeArea(Rect r)
+	{
+		if (!_rectTransform)
+			_rectTransform = GetComponent<RectTransform>();
+		
+		_lastSafeArea = r;
+		
+		Vector2 anchorMin = r.position;
+		Vector2 anchorMax = r.position + r.size;
+		anchorMin.x /= Screen.width;
+		anchorMin.y /= Screen.height;
+		anchorMax.x /= Screen.width;
+		anchorMax.y /= Screen.height;
+		
+		if (anchorMax.x > 1.0f || anchorMax.y > 1.0f)
+			anchorMax = UnitVector;
+		
+		_rectTransform.anchorMin = anchorMin;
+		_rectTransform.anchorMax = anchorMax;
+	}
 }
